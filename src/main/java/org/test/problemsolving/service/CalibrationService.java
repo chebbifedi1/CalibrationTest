13400e638d394ec87354aff867fe5f676f6e4de1
Fedi Chebbi <chebbi.fedi@esprit.tn>
2025-01-14 23:07:40 +0100

    fix single number

--- src/main/java/org/test/problemsolving/service/CalibrationService.java
+++ src/main/java/org/test/problemsolving/service/CalibrationService.java
@@ -13,17 +13,17 @@ public class CalibrationService {
     public int calculateCalibrationSum(String filePath) throws IOException {
         int total = 0;
         List<String> lines = Files.readAllLines(Path.of(filePath));
-
         for (String line : lines) {
             line = line.trim();
-
             String digits = line.replaceAll("[^0-9]", "");
             if (digits.length() >= 2) {
                 int calibrationValue = Integer.parseInt("" + digits.charAt(0) + digits.charAt(digits.length() - 1));
                 total += calibrationValue;
+            } else if (digits.length() == 1) {
+                int calibrationValue = Integer.parseInt(digits + digits); // Repeat the digit
+                total += calibrationValue;
             }
         }
-
         return total;
     }
 }
